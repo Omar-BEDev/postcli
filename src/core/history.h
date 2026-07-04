de1f378b7ca6c039716940cd8783cb3b21851fb1
Omar-BEDev <kaliochibrahim@gmail.com>
2026-07-04 15:06:34 +0000

    chore : make date struct and handlers headers

--- src/core/history.h
+++ src/core/history.h
@@ -14,4 +14,8 @@ typedef struct Date{
     int month;
     int day;
 } Date;
+
+Date str_to_date(char *date);
+int is_date_bigger(char *current_date, char *date);   
+int is_date_small(char *current_date, char *date);   
 #endif
