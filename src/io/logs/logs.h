64f14f10b9ec9766cb051b5f7975e4facff71681
Omar-BEDev <kaliochibrahim@gmail.com>
2026-07-09 10:10:50 +0000

    chore(extract_logs): add extract log declaration

--- src/io/logs/logs.h
+++ src/io/logs/logs.h
@@ -1,7 +1,8 @@
 #ifndef LOGS
 #define LOGS
+#include "../../utils/dynamic-array.h"
 // log format : ex "2026/07/07 new request" 
 #define DATE_LENGTH 10
 #define FIRST_MESSAGE_INDERX_POSITION 11
-
+void extract_logs(HistoryArray *history_arr, char *path);
 #endif
